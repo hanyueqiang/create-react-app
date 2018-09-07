d993c6d75735ee10a378d09351cd7f081d89f1ac
hanyueqiang <webyueqiang@163.com>
2018-09-07 11:44:36 +0800

    使用easy-mock获取数据填充表格

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,4 +1,6 @@
 import JsonP from 'jsonp'
+import { Modal } from 'antd';
+import axios from 'axios'
 export default class Axios {
     static JsonP(options) {
         return new Promise((resolve, reject)=>{
@@ -9,6 +11,32 @@ export default class Axios {
                     resolve(response);
                 }else {
                     reject(response.message);
+                }
+            })
+        })
+    }
+    static ajax(options) {
+        let baseApi = 'https://www.easy-mock.com/mock/5b90fcecce624c454133ee5b/bikeapi';
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: 'get',
+                baseURL: baseApi,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ''
+            }).then((response) => {
+                if(response.status == '200') {
+                    let res = response.data;
+                    if(res.code == '0') {
+                        resolve(res);
+                    }else {
+                        Modal.info({
+                            title: '提示',
+                            content: res.msg
+                        })
+                    }
+                }else {
+                    reject(response.data);
                 }
             })
         })

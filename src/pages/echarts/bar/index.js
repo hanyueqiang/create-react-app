deaadcce1166fbd1322a5969d0b8dbc93520f672
hanyueqiang <webyueqiang@163.com>
2018-10-26 23:19:32 +0800

    echarts

--- src/pages/echarts/bar/index.js
+++ src/pages/echarts/bar/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Card } from 'antd';
-import echartsTheme from './../echartTheme'
-import echarts from 'echarts/lib/echarts'
+//import echartsTheme from './../echartTheme'
+// import echarts from 'echarts/lib/echarts'
+
+// import 'echarts/lib/chart/bar'
+// import 'echarts/lib/component/tooltip'
+// import 'echarts/lib/component/title'
+// import 'echarts/lib/component/legend'
+// import 'echarts/lib/component/markPoint'
+// import ReactEcharts from 'echarts-for-react'
+import echarts from 'echarts'
 export default class Bar extends React.Component {
 
     componentDidMount() {
-        echarts.registerTheme('Bike', echartsTheme);
+        let myChart = echarts.init(document.getElementById('charts'));
+        myChart.setOption({
+            title: {
+                text: '骑行订单'
+            },
+            tooltip: {},
+            xAxis: {
+                data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+            },
+            yAxis: {},
+            series: [{
+                name: '数量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20, 25]
+            }]
+        });
+        //echarts.registerTheme('Bike', echartsTheme);
     }
     getOptions = () => {
         let option = {
@@ -31,7 +55,7 @@ export default class Bar extends React.Component {
     render() {
         return(
             <Card title='柱形图表1'>
-                
+                <div id="charts" style={{height:400}}></div>
             </Card>
         )
     }
